7eaf5b2490bf372ca0c81f0c0b29e6d808b5dab1
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-11-30 20:10:54 +0100

    get_slope : always return +inf for perpendicular lines
    before it could return -inf if you switched start and end
    this makes it easier to check if 2 perpendicular lines have the same slope

--- srcs/math_utils.c
+++ srcs/math_utils.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 19:51:23 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2023/11/30 19:52:41 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2023/11/30 20:10:37 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ t_slope	get_slope(const t_line *line)
 
 	slope.m = fabs(line->start.y - line->end.y)
 		/ fabs(line->start.x - line->end.x);
+	if (slope.m == -INFINITY)
+		slope.m = INFINITY;
 	slope.b = line->start.y - line->start.x * slope.m;
 	return (slope);
 }
