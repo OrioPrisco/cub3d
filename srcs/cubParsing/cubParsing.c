79ba4d780b61793edfd27e2948984240743e4e06
Mallory PEULET <mpeulet@gmail.com>
2023-12-01 16:05:51 +0100

    Week end save

--- srcs/cubParsing/cubParsing.c
+++ srcs/cubParsing/cubParsing.c
@@ -6,8 +6,24 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 13:45:59 by mpeulet           #+#    #+#             */
-/*   Updated: 2023/11/29 14:33:18 by mpeulet          ###   ########.fr       */
+/*   Updated: 2023/12/01 14:46:59 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	is_file_readable(char *file)
+{
+	if (open(file, O_RDONLY))
+		return (perror("Opening file "), 0);
+}
+
+int	open_cub(char *av)
+{
+	if (open(av, O_RDONLY))
+		return (perror("Opening file "), 1);	
+}
--- srcs/main.c
+++ srcs/main.c
@@ -3,13 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 15:24:35 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2023/11/28 15:24:46 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/12/01 14:52:10 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "cub3d.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int	is_file_readable(char *file)
 {
+	if (open(file, O_RDONLY) == -1)
+		return (perror("Opening file "), 0);
+    return (1);
+}
+
+int	main(int ac, char **av)
+{
+    if (ac != 2)
+        return (printf("Wrong arg\n"), 1);
+    if (!is_file_readable(av[1]))
+        return (1);
+    return (0);
 }
