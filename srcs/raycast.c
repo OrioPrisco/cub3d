a05865f1f915dabb585263c899cd002d188abc5e
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-12-05 13:09:20 +0100

    Fix typos
    intesect -> intersect

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -29,7 +29,7 @@ static bool	ray_looks_at_point(const t_line *ray, const t_point *point)
 			&& is_between(ray->start.y, point->y, ray->end.y)));
 }
 
-bool	ray_line_intesect(t_point *out_point,
+bool	ray_line_intersect(t_point *out_point,
 	const t_line *line, const t_line *ray)
 {
 	t_slope	slope_line;
@@ -57,7 +57,7 @@ bool	ray_line_intesect(t_point *out_point,
 }
 
 //returns index of line (-1 for none) and the intersection point
-int	ray_lines_intesect(t_point *out_point,
+int	ray_lines_intersect(t_point *out_point,
 	const t_line *line, const t_line *ray, size_t n)
 {
 	t_point	best;
@@ -70,7 +70,7 @@ int	ray_lines_intesect(t_point *out_point,
 	i = 0;
 	while (i < n)
 	{
-		if (ray_line_intesect(&current, line + i, ray))
+		if (ray_line_intersect(&current, line + i, ray))
 		{
 			if (distance2(&ray->start, &current)
 				< distance2(&ray->start, &best))
@@ -101,7 +101,7 @@ int main()
 	t_line ray = {{0,0}, {1, 1}};
 	t_point point = {-1,-1};
 	int foo;
-	foo=ray_lines_intesect(&point, &lines[0], &ray, sizeof(lines)/sizeof(lines[0]));
+	foo=ray_lines_intersect(&point, &lines[0], &ray,sizeof(lines)/sizeof(lines[0]));
 	printf("intersec : %d\npoint : %lf;%lf\n", foo, point.x, point.y);
 }
 */
