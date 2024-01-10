688d6bab8af08ee2930bfbe0e713ee4c1186b290
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-10 21:37:46 +0100

    Add draw_column
    had to pack some arguments due to the norm
    There might be a better way to do this by splitting responsability pehraps ?

---
+++ srcs/draw.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 18:20:04 by OrioPrisco        #+#    #+#             */
+/*   Updated: 2024/01/10 20:24:26 by OrioPrisco       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "image.h"
+#include "raycast.h"
+#include "math_utils.h"
+#include "vector.h"
+#include "draw.h"
+
+//TODO : color using texture, ceiling and floor color
+void	draw_column(t_column *column, const t_line *ray,
+			const t_line *look, const t_vector *lines)
+{
+	int		j;
+	double	dist;
+	double	draw_height;
+	int		hit;
+	t_point	point;
+
+	j = 0;
+	hit = ray_lines_intersect(&point, lines->data, ray, lines->size);
+	dist = -1;
+	if (hit >= 0)
+	{
+		dist = orth_distance(look, &point);
+		draw_height = column->image->height / dist;
+	}
+	while (j < column->image->height)
+	{
+		if (hit == -1
+			|| j * 2 >= column->image->height + draw_height
+			|| j * 2 <= column->image->height - draw_height)
+			my_mlx_pixel_put(column->image, column->col, j, 0x00);
+		else
+			my_mlx_pixel_put(column->image, column->col, j,
+				0xFF << ((hit % 5) * 4));
+		j++;
+	}
+}
