628f2c5df199f6ab7f2e783613ba4bdad5fd870e
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-11 15:54:54 +0100

    Add draw_screen
    calls draw_column in a loop

--- includes/draw.h
+++ includes/draw.h
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 20:18:16 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2024/01/10 23:21:12 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2024/01/11 15:35:36 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 # include <stddef.h>
 
+
 typedef struct s_img	t_img;
 typedef struct s_line	t_line;
 typedef struct s_vector	t_vector;
+typedef struct s_player	t_player;
 
 typedef struct s_column {
 	t_img	*image;
@@ -26,5 +28,7 @@ typedef struct s_column {
 
 void	draw_column(t_column column, t_line ray,
 			t_line look, const t_vector *lines);
-
+void	draw_screen(t_img *image, const t_player *player,
+			const t_vector *lines,
+			const double angles[*]);
 #endif
