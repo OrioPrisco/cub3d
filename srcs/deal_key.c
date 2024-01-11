9aa3301599130d13e228e2b2d9f2927a05a78a79
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-11 16:41:55 +0100

    Tweak deal_key values
    Values are now more reasonable for real time gameplay

--- srcs/deal_key.c
+++ srcs/deal_key.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 21:51:37 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2024/01/11 00:26:00 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2024/01/11 16:35:02 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,25 @@
 #undef __STRICT_ANSI__
 #include "math.h"
 
+#define STEP 0.1
+
 int	deal_key(int key, t_env *env)
 {
-	if (key == XK_Q || key == XK_Left)
+	if (key == XK_q || key == XK_Left)
 		env->player.look = vec2d_rotate(env->player.look, -M_PI / 180);
-	if (key == XK_E || key == XK_Right)
+	if (key == XK_e || key == XK_Right)
 		env->player.look = vec2d_rotate(env->player.look, M_PI / 180);
-	if (key == XK_W || key == XK_Up)
-		env->player.pos = point_add_vec2d(env->player.pos, env->player.look);
-	if (key == XK_S || key == XK_Down)
+	if (key == XK_w || key == XK_Up)
+		env->player.pos = point_add_vec2d
+			(env->player.pos, vec2d_mul(env->player.look, STEP));
+	if (key == XK_s || key == XK_Down)
 		env->player.pos = point_add_vec2d(env->player.pos,
-				vec2d_rotate(env->player.look, M_PI));
-	if (key == XK_A)
+				vec2d_mul(vec2d_rotate(env->player.look, M_PI), STEP));
+	if (key == XK_a)
 		env->player.pos = point_add_vec2d(env->player.pos,
-				vec2d_rotate(env->player.look, -M_PI / 2));
-	if (key == XK_D)
+				vec2d_mul(vec2d_rotate(env->player.look, -M_PI / 2), STEP));
+	if (key == XK_d)
 		env->player.pos = point_add_vec2d(env->player.pos,
-				vec2d_rotate(env->player.look, M_PI / 2));
+				vec2d_mul(vec2d_rotate(env->player.look, M_PI / 2), STEP));
 	return (0);
 }
