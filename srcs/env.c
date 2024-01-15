6021332291c8edf28b92639ed65aaa575739f251
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-15 14:21:27 +0100

    init_env/destroy_env : handle vectors
    init and clear the vectors in the env

--- srcs/env.c
+++ srcs/env.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 15:29:17 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2024/01/09 19:14:28 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2024/01/15 14:54:53 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -18,6 +18,9 @@ int	init_env(t_env *env)
 {
 	env->mlx = mlx_init();
 	env->frame = &env->frame1;
+	vector_init(&env->lines, sizeof(t_line));
+	vector_init(&env->graphics.textures, sizeof(t_img));
+	vector_init(&env->graphics.line_textures_id, sizeof(size_t));
 	if (!env->mlx)
 		return (1);
 	env->win = mlx_new_window(env->mlx, WIDTH, HEIGHT, "Cub3d");
@@ -39,12 +42,24 @@ void	switch_frame(t_env *env)
 
 void	destroy_env(t_env *env)
 {
+	size_t	i;
+	t_img	*image;
+
+	vector_clear(&env->lines);
+	vector_clear(&env->graphics.line_textures_id);
 	if (!env->mlx)
 		return ;
 	if (env->frame1.img)
 		mlx_destroy_image(env->mlx, env->frame1.img);
 	if (env->frame2.img)
 		mlx_destroy_image(env->mlx, env->frame2.img);
+	i = 0;
+	while (i < env->graphics.textures.size)
+	{
+		image = ((t_img *)env->graphics.textures.data) + i;
+		mlx_destroy_image(env->mlx, image->img);
+		i++;
+	}
 	mlx_destroy_display(env->mlx);
 	free(env->mlx);
 	return ;
