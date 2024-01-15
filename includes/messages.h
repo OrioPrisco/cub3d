59b4a0dc8d2049f3ebac1467e8f2a5c295005d58
Mallory PEULET <mpeulet@gmail.com>
2024-01-15 17:24:24 +0100

    fixed norm errors

--- includes/messages.h
+++ includes/messages.h
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 13:48:17 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/01/11 15:57:56 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/01/15 17:20:49 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # define INVALID_CUB "Error\nUnable to retrieve .cub datas\n"
 # define INVALID_MAP "Invalid map\n"
 
-# define INVALID_COMPASS_ID "Error/n\033[0;91mInvalid compass identifier in the .cub file\n\033[0;39m"
-# define INVALID_XPM "Error/n\033[0;91mInvalid extension expected for texture\n\033[0;39m"
+# define INVALID_COMPASS_ID "Error/n\033[0;91mInvalid compass identifier \
+in the .cub file\n\033[0;39m"
+# define INVALID_XPM "Error/n\033[0;91mInvalid extension expected \
+for texture\n\033[0;39m"
 
 #endif
