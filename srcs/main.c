c90de4141aa15f70eddbf599b5fc59e7990ed545
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-23 18:01:18 +0100

    Main : use map_to_lines

--- srcs/main.c
+++ srcs/main.c
@@ -18,10 +18,13 @@
 #include "draw.h"
 #include "hooks.h"
 #include <X11/X.h>
+#include "map.h"
+#include <stdlib.h>
 
 //#define WIDTH 210
 //#define HEIGHT 100
 
+/*
 static const t_line	g_lines[] = {
 {{-11, 10}, {11, 10}},
 {{10, 11}, {10, -11}},
@@ -31,7 +34,7 @@ static const t_line	g_lines[] = {
 {{9, 9}, {9, 8}},
 {{9, -9}, {-8, -9}},
 {{-9, -9}, {-9, -8}},
-{{0,0}, {11,11}},
+{{0, 0}, {11, 11}},
 };
 
 static const size_t	g_num_lines = sizeof(g_lines) / sizeof(g_lines[0]);
@@ -47,6 +50,17 @@ static const size_t	g_texture_id[] = {
 	1,
 	1.
 };
+*/
+
+static char	*g_tab[] = {
+	"111111111",
+	"100000001",
+	"101111001",
+	"100001001",
+	"100000001",
+	"111111111",
+	NULL
+};
 
 int	main(void)
 {
@@ -55,14 +69,20 @@ int	main(void)
 
 	ft_bzero(&env, sizeof(env));
 	init_env(&env);
-	vector_copy_n(&env.lines, g_lines, g_num_lines, sizeof(g_lines[0]));
+	//vector_copy_n(&env.lines, g_lines, g_num_lines, sizeof(g_lines[0]));
 	load_xpm(env.mlx, "textures/ball.xpm", &tmp);
 	vector_append(&env.graphics.textures, &tmp);
 	load_xpm(env.mlx, "textures/test.xpm", &tmp);
 	vector_append(&env.graphics.textures, &tmp);
-	vector_copy_n(&env.graphics.line_textures_id, g_texture_id, g_num_lines, sizeof(g_texture_id[0]));
+	load_xpm(env.mlx, "textures/brick_wall.xpm", &tmp);
+	vector_append(&env.graphics.textures, &tmp);
+	load_xpm(env.mlx, "textures/test.xpm", &tmp);
+	vector_append(&env.graphics.textures, &tmp);
+	//vector_copy_n(&env.graphics.line_textures_id, g_texture_id, g_num_lines, sizeof(g_texture_id[0]));
+	if (map_to_lines(g_tab, &env.lines, &env.graphics.line_textures_id, (t_point){9,6}))
+		exit(100);
 	env.player.look.x = 1;
-	env.bonus = true;
+	env.bonus = false;//true;
 	env.graphics.ceil_col = 0xFF;
 	env.graphics.floor_col = 0xFF00;
 	calculate_angles(WIDTH, env.angles, 90, 1);
