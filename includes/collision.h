4878f54281c442577de4490e76beb24a2edad85a
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-01-25 14:38:34 +0100

    Slightly increase player speed
    Increasing the player speed rom 0.1 to 0.15 makes collision slightly more accurate
    This might be a bit too fast when playing on certain computers, articial delay might be necessary

--- includes/collision.h
+++ includes/collision.h
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 13:04:37 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2024/01/17 17:13:58 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2024/01/25 14:34:32 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define COLLISION_H
 # include "math_utils.h"
 
-# define STEP 0.1
+# define STEP 0.15
 
 typedef struct s_player	t_player;
 typedef struct s_vector	t_vector;
