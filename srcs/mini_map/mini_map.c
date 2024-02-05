7f0133559ca852713e0d5465835682450289d13d
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-02-05 11:08:22 +0100

    Minimap : rotate along player

--- srcs/mini_map/mini_map.c
+++ srcs/mini_map/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 13:36:45 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/02 15:28:05 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2024/02/05 11:08:01 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "image.h"
 #include "map.h"
 #include "mini_map.h"
+#include <math.h>
 
 static int	char_to_color(char c)
 {
@@ -36,21 +37,26 @@ static char	char_at_map(const t_map *map, int y, int x)
 void	render_mini_map(t_img *mini,
 			const t_map *map, const t_player *player, double blocks)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	t_vec2d	rotated;
+	double	angle;
 
 	i = -1;
+	angle = atan2(player->look.x, -player->look.y);
 	while (++i < mini->height)
 	{
 		j = 0;
 		while (j < mini->width)
 		{
+			rotated = vec2d_rotate((t_vec2d){
+					(((double)j) / mini->width - .5) * blocks,
+					(((double)i) / mini->height - .5) * blocks
+					* mini->height / mini->width}, angle);
 			my_mlx_pixel_put(mini, j, i, char_to_color(char_at_map(map,
-						(((double)i) / mini->height - .5)
-						* blocks / 2 + player->pos.y,
-						(((double)j) / mini->width - .5)
-						* blocks / 2 * mini->width / mini->height
-						+ player->pos.x)));
+						rotated.y + player->pos.y,
+						rotated.x + player->pos.x
+						)));
 			j++;
 		}
 	}
