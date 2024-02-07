d3d8912ec9b58cb2c54c6d997ce75d6427f131b6
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-02-07 17:48:04 +0100

    Add destroy_img
    convenient wrapper

--- srcs/image2.c
+++ srcs/image2.c
@@ -6,11 +6,12 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:21:02 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2024/01/12 18:13:17 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2024/02/07 17:47:29 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "image.h"
+#include "mlx.h"
 
 int	get_pixel(const t_img *img, int x, int y)
 {
@@ -18,4 +19,10 @@ int	get_pixel(const t_img *img, int x, int y)
 
 	dest = img->data + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	return (*(int *)dest);
+}
+
+void	destroy_img(void *mlx, t_img *img)
+{
+	if (img && img->img)
+		mlx_destroy_image(mlx, img->img);
 }
